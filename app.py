f58c7a73d3e89d2dd22cd5771d0418969956d62e
keiras1990-cloud <keiras1990@gmail.com>
2026-05-22 21:35:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,11 +46,11 @@ if choice == "🔍 Pengesan Scam":
             with st.spinner("AntiLolos AI sedang mengimbas corak penipuan..."):
                 
                 # Enjin Caching Supabase
-                db_query = supabase.table("scam_logs").select("*").eq("Teks_Laporan", user_input.strip()).execute()
+                db_query = supabase.table("scam_logs").select("*").eq("teks_laporan", user_input.strip()).execute()
                 
                 if db_query.data:
-                    status = db_query.data[0]["Klasifikasi_Gemini"]
-                    ulasan = db_query.data[0]["Ulasan_AI"]
+                    status = db_query.data[0]["klasifikasi_gemini"]
+                    ulasan = db_query.data[0]["ulasan_ai"]
                     st.caption("💡 Hasil semakan pantas ditemui dalam memori pangkalan data komuniti.")
                 else:
                     try:
@@ -68,9 +68,9 @@ if choice == "🔍 Pengesan Scam":
                         ulasan = output_text.replace("KATEGORI: SCAM BAHAYA", "").replace("KATEGORI: SELAMAT", "").strip()
                         
                         supabase.table("scam_logs").insert({
-                            "Teks_Laporan": user_input.strip(),
-                            "Klasifikasi_Gemini": status,
-                            "Ulasan_AI": ulasan
+                            "teks_laporan": user_input.strip(),
+                            "klasifikasi_gemini": status,
+                            "ulasan_ai": ulasan
                         }).execute()
                     except Exception as e:
                         st.error("Sistem mengalami kelengahan teknikal.")
